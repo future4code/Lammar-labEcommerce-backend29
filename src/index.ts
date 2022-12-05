7de28cff4a2a59a9e4d2792c6211ec17bf45e207
unknown <neiluisjr@gmail.com>
2022-12-04 21:21:34 -0300

    Tudo ok

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import getAllCharacters from "./endpoints/getAllCharacters";
 import app from "./app";
 import createCharacter from "./endpoints/createCharacter";
+import deleteCharacter from "./endpoints/deleteCharacter";
 
 app.get("/character", getAllCharacters)
 app.put("/character", createCharacter)
+app.delete("/character/:id", deleteCharacter)
